568c47ebd9e33261c2048fc12b248abce2bf7efd
trofimenko-anton <shirinkin.anton@gmail.com>
2026-06-15 20:22:51 +0300

    code changes

--- lesson_05/test_lesson05_task2.py
+++ lesson_05/test_lesson05_task2.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def test_form_submission():
@@ -10,16 +10,15 @@ def test_form_submission():
     old_url = driver.current_url
 
     name_field = driver.find_element(By.NAME, "custname")
-    name_field.send_keys("Антон Трофименко")
+    name_field.send_keys("Anton")
 
     submit_button = driver.find_element(
-        By.XPATH,
-        "//button[text()='Submit order']"
-    )
+        By.XPATH, "//button[text()='Submit order']")
     submit_button.click()
 
-    time.sleep(3)
+    wait = WebDriverWait(driver, 5)
+    wait.until(lambda driver: driver.current_url != old_url)
 
-    assert driver.current_url != old_url
+    assert driver.current_url == "https://httpbin.org/post"
 
     driver.quit()
--- lesson_05/test_lesson05_task3.py
+++ lesson_05/test_lesson05_task3.py
@@ -1,17 +1,16 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
+@pytest.mark.xfail(reason="На странице 9 ссылок вместо ожидаемых 10")
 def test_multiple_elements():
     driver = webdriver.Chrome()
     driver.get("https://httpbin.org/links/10")
 
-    links = driver.find_elements(By. TAG_NAME, "a")
+    links = driver.find_elements(By.TAG_NAME, "a")
 
-    assert len(links) == 9
-    # Ожидаем 9 ссылок, а не 10, как указано в задании
-    # При выполнении теста Selenium не находит 10 ссылку
-    # Страница загружается асинхронно
+    assert len(links) == 10
 
     for link in links:
         assert link.is_displayed()
